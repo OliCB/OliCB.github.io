d82445682c24142d487e2f7f6c3eea3dda8b439d
Olivier Couture-Bienvenue <misterolichip@gmail.com>
2024-02-18 14:31:34 -0500

    fix desktop design

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -3,19 +3,18 @@ document.querySelectorAll('.row').forEach(r => {
 });
 
 function toggleRow() {
-    const rowBody = this.querySelector('.card__row-text');
+    const rowBody = this.querySelector('.card__row-text-container');
     const img = this.querySelector('img');
     const opened = rowBody.dataset.opened == "true";
 
     if (!opened) {
         rowBody.style.maxHeight = `${rowBody.scrollHeight}px`;
-        rowBody.classList.add('card__row-text--active');
         img.src = 'assets/images/icon-minus.svg';
         rowBody.dataset.opened = "true";
     }
     else {
         rowBody.style.maxHeight = null;
-        rowBody.classList.remove('card__row-text--active');
+
         img.src = 'assets/images/icon-plus.svg';
         rowBody.dataset.opened = "false";
     }
