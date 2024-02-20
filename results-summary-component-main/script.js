7a8088e180c31189e89e5e92c39f254e4e62a8ac
Olivier Couture-Bienvenue <misterolichip@gmail.com>
2024-02-20 17:00:33 -0500

    load data dynamically

--- results-summary-component-main/script.js
+++ results-summary-component-main/script.js
@@ -1,5 +1,24 @@
 document.addEventListener('DOMContentLoaded', onInit);
 
-function onInit() {
-    
+async function onInit() {
+    let scores = await fetch('data.json').then(data => data.json());
+    let rowContainer = document.querySelector('.results__summary-rows');
+    let colors = [
+        'results--red', 
+        'results--yellow', 
+        'results--green', 
+        'results--blue', 
+    ];
+
+    rowContainer.innerHTML = scores.map((s, i) =>  `
+        <div class="results__summary-row ${colors[i % colors.length]}">
+            <div class="results__summary-category">
+                <img src="${s.icon}" alt="">
+                ${s.category}
+            </div>
+            <div class="results__summary-score">
+                <span class="results__summary-score--hl">${s.score}</span> / 100
+            </div>
+        </div>   
+    `).join('');
 }
